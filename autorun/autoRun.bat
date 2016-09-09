f6c1003ad29ec600895a8972808514bd9c7113d7
Administrator <632663267@qq.com>
2016-09-09 16:49:39 +0800

    aaaa

--- autorun/autoRun.bat
+++ autorun/autoRun.bat
@@ -3,6 +3,6 @@ cls
 :start
 cd ..
 set APP_HOME=%cd%
-copy D:\space-private\customer\target\customer-1.0.jar %APP_HOME%\autorun\customer-1.0.jar
+copy %APP_HOME%\target\customer-1.0.jar %APP_HOME%\autorun\customer-1.0.jar
  java -Dfile.encoding=utf-8 -jar %APP_HOME%\autorun\customer-1.0.jar %c%
 goto :start
