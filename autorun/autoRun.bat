61953251e64df1031987057594e589e73ed9b343
Martin Zhang <632663267@qq.com>
2016-10-25 10:57:57 +0800

    create

--- autorun/autoRun.bat
+++ autorun/autoRun.bat
@@ -3,6 +3,6 @@ cls
 :start
 cd ..
 set APP_HOME=%cd%
-copy %APP_HOME%\target\customer-1.0.jar %APP_HOME%\autorun\customer-1.0.jar
- java -Dfile.encoding=utf-8 -jar %APP_HOME%\autorun\customer-1.0.jar %c%
+copy %APP_HOME%\target\examSystem-1.0.jar %APP_HOME%\autorun\examSystem-1.0.jar
+ java -Dfile.encoding=utf-8 -jar %APP_HOME%\autorun\examSystem-1.0.jar %c%
 goto :start
